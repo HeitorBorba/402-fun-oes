909af7474618238e1b2629f5a695e2cb618e3b98
heitor <heitorbc3@gmail.com>
2019-07-23 17:29:20 -0300

    Adicionar funçoes

---
+++ script.js
@@ -0,0 +1,49 @@
+// let compras = ['salgadinho', 'coca-cola', 'bolo', 'macarrao', 'peixe']; {
+
+//     alert(compras);
+// }
+
+// // Adiionar cebola
+
+// compras.push('cebola'); {
+
+//     alert(compras);
+// }
+
+// // Remover cebola
+
+// compras.pop('cebola'); {
+
+//     alert(compras);
+// }
+
+
+// function somar(num1 , num2) {
+//    alert(num1 + num2)
+
+// }
+
+// somar(3,5);
+// somar(10,4);
+// somar(1,4);
+
+// let corpo = document.querySelector("body");
+// let botao = document.querySelector('button');
+// window.confirm('esta pagina foi atualizada');
+// function cor() {
+//     corpo.style.backgroundColor = "blue";
+//     window.alert("A cor foi trocada para azul");
+// }
+// botao.onclick = cor
+
+
+let email2 = document.querySelector(#email);
+let senha2 = document.querySelector(#senha);
+
+function valdiarlogin(){
+    if(email2.value == 'heitorbc3@gmail.com' && senha2.value == '123teste'){
+       
+    }
+    
+}
+
